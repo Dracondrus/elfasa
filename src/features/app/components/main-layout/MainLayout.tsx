e5e9e9c42b52461ebb95424c56ecb4e10bb67cdc
ELFASA <alucardmobilephone@gmail.com>
2025-02-09 02:09:51 +0500

    qweqwssss

--- src/features/app/components/main-layout/MainLayout.tsx
+++ src/features/app/components/main-layout/MainLayout.tsx
@@ -1,62 +1,39 @@
 import {  Outlet } from "react-router-dom"
 import Navigation from "../navigation/Navigation"
 import { useEffect, useState } from "react";
-import $api from "../../utils/helpers/axiosInstance";
-import axios from "axios";
 
 
-interface KeyModel {
-    id:number,
-    name: string;
+interface IKeys {
+    id:number;
+    name:string;
 }
-
 const MainLayout:React.FC = () => {
-    const [keys, setKeys] = useState<KeyModel[] | null>(null);
-    const [data, setData] = useState<KeyModel[] | null>(null);
-    useEffect(() => {
-        $api.get<KeyModel[]>("api/keys")
-            .then((response) => {
-                setKeys(response.data);
-               
-            })
-            .catch(() => {
-               console.log("")
-            })
-            .finally(() => {
-                console.log("")
-            });
-    }, []);
-
-
-
-
-    useEffect(() => {
-        axios.get<KeyModel[]>("https://f4d8-194-93-25-25.ngrok-free.app/api/keys")
-            .then((response) => {
-                setData(response.data);
-            })
-            .catch(() => {
-               console.log("")
-            })
-            .finally(() => {
-                console.log("")
-            });
-    }, []);
-
-console.log(keys)
-console.log(data)
+    const [keys,setKeys] = useState<IKeys[] >([] )
+  useEffect(() => {
+    fetch("https://1065-194-93-25-25.ngrok-free.app/api/keys", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "ngrok-skip-browser-warning": "true" // Добавляем этот заголовок
+        }
+    })
+    .then(response => response.json()) // Если сервер возвращает JSON
+    .then(data => {
+        console.log(data)
+        setKeys(data)
+    })
+    .catch(error => console.error("Ошибка:", error));
+  }, [])
 
     return (
         <div>
             LOGOTYPE 
-          
-            <Navigation/>
-            {keys?.map(item => (
-                <div key={item.name}>
-                    <h3>{item.id}</h3><br />
-                    <h3>{item.name}</h3><br />
-                </div>
+            {keys.map(item => (
+                <div key={item.id}>{item.name}</div>
             ))}
+          <br />
+            <Navigation/>
+          
             <br />
             
             <Outlet/>
